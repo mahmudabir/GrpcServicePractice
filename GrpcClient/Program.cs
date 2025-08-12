4e3919655ef4c40f03f7cba6ce9e3d8f730bcfb3
Abir <amabirmahmud@gmail.com>
2025-08-12 15:48:31 +0600

    Update Program.cs

--- GrpcClient/Program.cs
+++ GrpcClient/Program.cs
@@ -5,14 +5,56 @@ using GrpcServiceDemo;
 
 var channel = GrpcChannel.ForAddress("http://localhost:5001");
 var client = new Greeter.GreeterClient(channel);
+Console.WriteLine($"Application Stared At: {DateTime.Now}");
+Console.WriteLine();
 
+const int loopCount = 10_000_000;
+Console.WriteLine($"Loop count: {loopCount}");
+Console.WriteLine($"Loop Stared At: {DateTime.Now}");
+List<AsyncUnaryCall<HelloReply>> calls = new List<AsyncUnaryCall<HelloReply>>();
+for (int i = 0; i < loopCount; i++)
+{
+    AsyncUnaryCall<HelloReply> clientCall = client.SayHelloAsync(new HelloRequest
+    {
+        Name = "Abir"
+    });
+    calls.Add(clientCall);
+}
+Console.WriteLine($"Loop Ended At: {DateTime.Now}");
+Console.WriteLine();
+
+// Extract tasks from the calls when you need to await them
+var tasks = calls.Select(call => call.ResponseAsync);
+var results = await Task.WhenAll(tasks);
+Console.WriteLine(string.Join("\r", results.Select(result => result.Message)));
+Console.WriteLine();
+
+Console.WriteLine($"Loop Call await Ended At: {DateTime.Now}");
+Console.WriteLine();
+
+Console.WriteLine($"Single Call Started At: {DateTime.Now}");
 // Unary call
-var reply = await client.SayHelloAsync(new HelloRequest { Name = "Abir" });
+var reply = await client.SayHelloAsync(new HelloRequest
+{
+    Name = "Abir"
+});
 Console.WriteLine(reply.Message);
+Console.WriteLine($"Single Call Ended At: {DateTime.Now}");
+Console.WriteLine();
 
+Console.WriteLine($"Stream Call Started At: {DateTime.Now}");
 // Server streaming
-using var call = client.SayHelloStream(new HelloRequest { Name = "Abir" });
+using var call = client.SayHelloStream(new HelloRequest
+{
+    Name = "Abir"
+});
 await foreach (var response in call.ResponseStream.ReadAllAsync())
 {
     Console.WriteLine(response.Message);
 }
+Console.WriteLine($"Stream Call Ended At: {DateTime.Now}");
+Console.WriteLine();
+
+
+Console.WriteLine($"Application Ended At: {DateTime.Now}");
+Console.WriteLine();
